3681f61b13c693942cab489e04d78cac23753237
hanyu <hanyu2@asu.edu>
2017-07-21 12:25:38 -0700

    387

--- src/d721/FindKPairsWithSmallestSums.java
+++ src/d721/FindKPairsWithSmallestSums.java
@@ -35,13 +35,3 @@ public class FindKPairsWithSmallestSums {
 	}
 }
 
-class Tuple{
-	int x;
-    int y;
-    int val;
-    public Tuple(int x, int y, int val) {
-		this.x = x;
-		this.y = y;
-		this.val = val;
-	}
-}
--- src/d721/KthSmallestElementInASortedMatrix.java
+++ src/d721/KthSmallestElementInASortedMatrix.java
@@ -1,7 +1,6 @@
 package d721;
 
 import java.util.ArrayList;
-import java.util.Arrays;
 import java.util.Collections;
 import java.util.List;
 import java.util.PriorityQueue;
@@ -25,7 +24,11 @@ public class KthSmallestElementInASortedMatrix {
         }
         return pq.poll();
     }
-	//binary search
+	//binary search O(m*n*logk)
+	//1.开始的判断条件写的是如果，list长度小于k，直接add，会造成list顺序混乱
+	//[1, 2][1, 3]造成[1, 2, 1]
+	//2.注意判断条件list.size()-1，当list为空，越界
+	//不能在前面加list为空的判断，会造成无法填满k个元素
 	public static int kthSmallest2(int[][] matrix, int k) {
 		List<Integer> list = new ArrayList<Integer>();
 		int m = matrix.length;
@@ -46,8 +49,31 @@ public class KthSmallestElementInASortedMatrix {
 		}
 		return list.get(list.size() - 1);
 	}
+	
+	//类似373
+	public int kthSmallest4(int[][] matrix, int k) {
+        int n = matrix.length;
+        PriorityQueue<Tuple> pq = new PriorityQueue<Tuple>();
+        for(int j = 0; j <= n-1; j++) pq.offer(new Tuple(0, j, matrix[0][j]));
+        for(int i = 0; i < k-1; i++) {
+            Tuple t = pq.poll();
+            if(t.x == n-1) continue;
+            pq.offer(new Tuple(t.x+1, t.y, matrix[t.x+1][t.y]));
+        }
+        return pq.poll().val;
+    }
 	public static void main(String[] args) {
 		int[][] matrix = {{1,2},{1,3}};
 		System.out.println(kthSmallest2(matrix, 3));
+	}
+}
+class Tuple{
+	int x;
+    int y;
+    int val;
+    public Tuple(int x, int y, int val) {
+		this.x = x;
+		this.y = y;
+		this.val = val;
 	}
 }
